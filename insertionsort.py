2c27d122ec5d7fcf18f4eb63f0c6d68fbf3d0b2f
Rishitha Janjirala <rishithajanjirala@gmail.com>
2025-07-27 10:47:31 +0530

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,5 +1,5 @@
 #defination-takes an element and place at correct position
-def insertion(arr,n):#T.C-	O(n²),S.C-O(1)
+def insertion(arr,n):#T.C-O(n²),S.C-O(1)
     for i in range(0, n):
         # Move the current element to its correct position by swapping with previous larger elements
         while i > 0 and arr[i - 1] > arr[i]:
