84f91d9cb9dd945e882262a81b65dcc6560ea5cd
Rishitha Janjirala <rishithajanjirala@gmail.com>
2025-07-27 10:28:47 +0530

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,10 +1,13 @@
 #defination-takes an element and place at correct position
 def insertion(arr,n):
-    for i in range(0,n):
-        while i>0 and arr[i-1]>arr[i]:
-            arr[i-1],arr[i]=arr[i],arr[i-1]
-            i-=1
+    for i in range(0, n):
+        # Move the current element to its correct position by swapping with previous larger elements
+        while i > 0 and arr[i - 1] > arr[i]:
+            # Swap if the previous element is greater than current
+            arr[i - 1], arr[i] = arr[i], arr[i - 1]
+            i -= 1
     return arr
+       
 arr=list(map(int,input().split()))
 n=len(arr)
-print(insertion(arr,n))
+print('sorted array:',insertion(arr,n))
